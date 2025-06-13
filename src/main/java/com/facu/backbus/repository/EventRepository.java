da6aa2bfd1bad794966ce85dce53ad9c67899309
Dev-Wilk <programadorwilk@gmail.com>
2025-06-13 14:47:44 -0300

    fix: Implemente Repository: Eventt, User

--- src/main/java/com/facu/backbus/repository/EventRepository.java
+++ src/main/java/com/facu/backbus/repository/EventRepository.java
@@ -1,3 +1,9 @@
-public class EventRepository {
+package com.facu.backbus.repository;
 
+import com.facu.backbus.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
 }
--- src/main/java/com/facu/backbus/repository/UserRepository.java
+++ src/main/java/com/facu/backbus/repository/UserRepository.java
@@ -1,3 +1,12 @@
-public class UserRepository {
+package com.facu.backbus.repository;
 
+import com.facu.backbus.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);         
 }
