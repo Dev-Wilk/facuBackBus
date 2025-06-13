851fca6c7993fc3dd183104288ac59812823681a
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-06-13 12:18:13 -0300

    fix repository: BusRepository.java and DriverRepository.java

--- src/main/java/com/facu/backbus/repository/BusRepository.java
+++ src/main/java/com/facu/backbus/repository/BusRepository.java
@@ -1,3 +1,9 @@
-public class BusRepository {
+package com.facu.backbus.repository;
 
+import com.facu.backbus.model.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusRepository extends JpaRepository<Bus, Long> {
 }
--- src/main/java/com/facu/backbus/repository/DriverRepository.java
+++ src/main/java/com/facu/backbus/repository/DriverRepository.java
@@ -1,3 +1,9 @@
-public class DriverRepository {
+package com.facu.backbus.repository;
 
+import com.facu.backbus.model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
 }
