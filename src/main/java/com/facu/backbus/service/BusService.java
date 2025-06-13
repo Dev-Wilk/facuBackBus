12706c8b2f0766a376e5d4c91839499a671ca58b
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-06-13 12:22:17 -0300

    fix service: BusService.java and DriverService.java

--- src/main/java/com/facu/backbus/service/BusService.java
+++ src/main/java/com/facu/backbus/service/BusService.java
@@ -1,3 +1,45 @@
-public class BusService {
+package com.facu.backbus.service;
 
+import com.facu.backbus.model.Bus;
+import com.facu.backbus.repository.BusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BusService {
+    private final BusRepository busRepository;
+
+    @Autowired
+    public BusService(BusRepository busRepository) {
+        this.busRepository = busRepository;
+    }
+
+    public List<Bus> findAll() {
+        return busRepository.findAll();
+    }
+
+    public Optional<Bus> findById(Long id) {
+        return busRepository.findById(id);
+    }
+
+    public Bus save(Bus bus) {
+        return busRepository.save(bus);
+    }
+
+    public void deleteById(Long id) {
+        busRepository.deleteById(id);
+    }
+    
+    /**
+     * Busca todos os ônibus pelos seus IDs
+     * @param ids Lista de IDs dos ônibus a serem buscados
+     * @return Lista de ônibus encontrados
+     */
+    public List<Bus> findAllById(List<Long> ids) {
+        return busRepository.findAllById(ids);
+    }
 }
--- src/main/java/com/facu/backbus/service/DriverService.java
+++ src/main/java/com/facu/backbus/service/DriverService.java
@@ -1,3 +1,44 @@
-public class DriverService {
+package com.facu.backbus.service;
 
+import com.facu.backbus.model.Driver;
+import com.facu.backbus.repository.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DriverService {
+    private final DriverRepository driverRepository;
+
+    @Autowired
+    public DriverService(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    public List<Driver> findAll() {
+        return driverRepository.findAll();
+    }
+
+    public Optional<Driver> findById(Long id) {
+        return driverRepository.findById(id);
+    }
+
+    public Driver save(Driver driver) {
+        return driverRepository.save(driver);
+    }
+
+    public void deleteById(Long id) {
+        driverRepository.deleteById(id);
+    }
+    
+    /**
+     * Busca todos os motoristas pelos seus IDs
+     * @param ids Lista de IDs dos motoristas a serem buscados
+     * @return Lista de motoristas encontrados
+     */
+    public List<Driver> findAllById(List<Long> ids) {
+        return driverRepository.findAllById(ids);
+    }
 }
