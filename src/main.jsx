c92ac4f60d8926ea2e347d675108573c59a6bc95
Dev-Wilk <programadorwilk@gmail.com>
2025-06-26 00:09:32 -0300

    add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/GlobalStyles.css';
+import { BrowserRouter, useLocation } from 'react-router-dom';
+import Router from './router/Router.jsx';
+import NavBarWrapper from './components/navbar/NavBarWrapper.jsx';
+
+function AppLayout() {
+ 
+  const location = useLocation();
+
+
+  const hideNavbarRoutes = ['/'];
+  
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
+  return (
+    <>
+     {/* Se shouldHideNavBar for falso então ele renderiza o componente navBar, se for verdadeiro ele não renderiza */}
+      {!shouldHideNavbar && <NavBarWrapper />} 
+      <Router />
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AppLayout />
+    </BrowserRouter>
+  </StrictMode>
+);
