44e776a61e505d7c5ba884fc5675d6b5fcc4ea73
Dev-Wilk <programadorwilk@gmail.com>
2025-06-26 00:06:29 -0300

    add login  all

---
+++ src/pages/Login.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom'; 
+import api from '../services/Api';
+import '../styles/Login.css';
+
+function Login() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/api/auth/login', {
+        login,
+        password,
+      });
+
+      const usuario = response.data;
+      const tipo = usuario.userType.toUpperCase();
+
+      const credentials = btoa(`${login}:${password}`);
+      localStorage.setItem('tipoUsuario', tipo);
+      localStorage.setItem('usuarioLogado', usuario.fullName);
+      localStorage.setItem('authHeader', `Basic ${credentials}`);
+      api.defaults.headers.common['Authorization'] = `Basic ${credentials}`;
+
+      if (tipo === 'GERENTE') {
+        navigate('/dashboard-admin');
+      } else {
+        navigate('/dashboard');
+      }
+
+      window.location.reload();
+    } catch (error) {
+      console.error('Erro no login:', error);
+      alert('Login inválido. Verifique seu usuário e senha.');
+    }
+  };
+
+  return (
+    <div>
+      <title>Entrar</title>
+      <div className='ContainerLogin'>
+        <form className='FormLogin' onSubmit={handleLogin}>
+          <div className='DivLogo'>
+            <img className='LogoLogin' src="#" alt="logo" />
+          </div>
+
+          <div className='DivInpt'>
+            <input
+              className='InptLogin'
+              type="text"
+              required
+              placeholder='Login'
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+            />
+
+            <input
+              className='InptLogin'
+              type="password"
+              required
+              placeholder='Senha'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className='BtnLogin' type="submit">Entrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
